619e8760701d4e8b173d838fd7cb9391da1345d7
Squishich <segaa_mont@mail.ru>
2022-10-28 20:33:25 +0700

    kofemashina

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 def matem(rashet, centiki) :
-    if (rashet-centiki(bibor) == 0):
+    if (rashet-centiki[bibor]== 0):
         print('наливается кофе')
-    elif(rashet-centiki(bibor)>0):
-        print('наливается кофе','ваша сдача -',rashet-centiki(bibor))
+    elif(rashet-centiki[bibor]>0):
+        print('наливается кофе','ваша сдача -',rashet-centiki[bibor])
     else:
-        while(centiki(bibor)-rashet>0):
-            print('вложите ещё',centiki(bibor)-rashet)
-            rashet = int(input())
+        while(centiki[bibor]-rashet>0):
+            print('вложите ещё',centiki[bibor]-rashet)
+            rashet = int(input()) + rashet
+            if(centiki[bibor]-rashet<0):
+                print('кафе наливается,ваша сдача -',rashet-centiki[bibor])
 
 a = 'латте'
 b = 'капучино'
 c = 'американо'
 d = 'kakao'
-e = 'экспрессо'
+e = 'эспрессо'
 f = 'мокко'
 
 a2 = 100
@@ -30,17 +32,18 @@ for i in zip(nomerkofe,spisokKofe,centiki):
 
 print('выберете HoMep кофе:')
 bibor =int(input())
-if (bibor>6 or bibor<0):
-    print('неверный ввод')
-else:
-    print('ваш кофе -', spisokKofe[bibor-1])
-    print('выберете способ оплаты (1)безнал/(2)наличка')
-    oplata=int(input())
-if (oplata == 1):
-    print('кофе наливается')
-elif(oplata == 2):
-    print('внесите оплату')
-    rashet = int(input())
-    matem(rashet, centiki)
-else:
-    print('неверный способ оплаты')
+while(bibor!= 322):
+    if (bibor>6 or bibor<0):
+        print('неверный ввод')
+    else:
+        print('ваш кофе -', spisokKofe[bibor-1])
+        print('выберете способ оплаты (1)безнал/(2)наличка')
+        oplata=int(input())
+    if (oplata == 1):
+        print('кофе наливается')
+    elif(oplata == 2):
+        print('внесите оплату')
+        rashet = int(input())
+        matem(rashet, centiki)
+    else:
+        print('неверный способ оплаты')
